3af21149e8659fbd514ca776aa721a881449e056
qbj700 <qbj700@gmail.com>
2021-01-07 12:13:16 +0900

    DB 스키마 추가 / 회원 리스트 서블릿 구현

---
+++ src/com/sbs/example/jspCommunity/util/MysqlUtilException.java
@@ -0,0 +1,53 @@
+package com.sbs.example.jspCommunity.util;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+@SuppressWarnings("serial")
+public class MysqlUtilException extends RuntimeException {
+	private Exception originException;
+
+	public MysqlUtilException(Exception e) {
+		this.originException = e;
+	}
+
+	@Override
+	public String getMessage() {
+		return originException.getMessage();
+	}
+
+	@Override
+	public String getLocalizedMessage() {
+		return originException.getLocalizedMessage();
+	}
+
+	@Override
+	public synchronized Throwable getCause() {
+		return originException.getCause();
+	}
+
+	@Override
+	public StackTraceElement[] getStackTrace() {
+		return originException.getStackTrace();
+	}
+
+	@Override
+	public String toString() {
+		return originException.toString();
+	}
+
+	@Override
+	public void printStackTrace() {
+		originException.printStackTrace();
+	}
+
+	@Override
+	public void printStackTrace(PrintStream s) {
+		originException.printStackTrace(s);
+	}
+
+	@Override
+	public void printStackTrace(PrintWriter s) {
+		originException.printStackTrace(s);
+	}
+}
