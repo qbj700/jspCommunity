d16a5a12ec1242e288e48e8e383a289dbcfea57d
qbj700 <qbj700@gmail.com>
2021-01-19 12:07:24 +0900

    각 서블릿 중복코드 제거 / 템플릿 메서드 패턴 적용

--- src/com/sbs/example/jspCommunity/servlet/UsrDispatcherServlet.java
+++ src/com/sbs/example/jspCommunity/servlet/UsrDispatcherServlet.java
@@ -1,39 +1,17 @@
 package com.sbs.example.jspCommunity.servlet;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sbs.example.jspCommunity.container.Container;
 import com.sbs.example.jspCommunity.controller.UsrArticleController;
 import com.sbs.example.jspCommunity.controller.UsrMemberController;
-import com.sbs.example.jspCommunity.util.MysqlUtil;
 
 @WebServlet("/usr/*")
-public class UsrDispatcherServlet extends HttpServlet {
+public class UsrDispatcherServlet extends DispatcherServlet {
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		resp.setContentType("text/html; charset=UTF-8");
-
-		String requestUri = req.getRequestURI();
-		String[] requestUriBits = requestUri.split("/");
-
-		if (requestUriBits.length < 5) {
-			resp.getWriter().append("올바른 요청이 아닙니다.");
-			return;
-		}
-
-		String controllerName = requestUriBits[3];
-		String actionMethodName = requestUriBits[4];
-
-		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jspCommunity");
-
+	protected String doAction(HttpServletRequest req, HttpServletResponse resp, String controllerName, String actionMethodName) {
 		String jspPath = null;
 
 		if (controllerName.equals("member")) {
@@ -41,9 +19,9 @@ public class UsrDispatcherServlet extends HttpServlet {
 
 			if (actionMethodName.equals("list")) {
 				jspPath = memberController.showList(req, resp);
-			}else if (actionMethodName.equals("join")) {
+			} else if (actionMethodName.equals("join")) {
 				jspPath = memberController.showJoin(req, resp);
-			}else if (actionMethodName.equals("doJoin")) {
+			} else if (actionMethodName.equals("doJoin")) {
 				jspPath = memberController.doJoin(req, resp);
 			}
 		} else if (controllerName.equals("article")) {
@@ -66,15 +44,6 @@ public class UsrDispatcherServlet extends HttpServlet {
 			}
 
 		}
-
-		MysqlUtil.closeConnection();
-
-		RequestDispatcher rd = req.getRequestDispatcher("/jsp/" + jspPath + ".jsp");
-		rd.forward(req, resp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
+		return jspPath;
 	}
 }
