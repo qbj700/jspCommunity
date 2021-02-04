3e5492d36acd3cb6e51f33eda8cf6f46ddd206fe
qbj700 <qbj700@gmail.com>
2021-02-04 15:11:11 +0900

    DB 원격 연결 계정 설정

--- src/com/sbs/example/jspCommunity/servlet/DispatcherServlet.java
+++ src/com/sbs/example/jspCommunity/servlet/DispatcherServlet.java
@@ -61,7 +61,20 @@ public abstract class DispatcherServlet extends HttpServlet {
 			return null;
 		}
 
-		MysqlUtil.setDBInfo("127.0.0.1", "sbsst", "sbs123414", "jspCommunity");
+		String profilesActive = System.getProperty("spring.profiles.active");
+		
+		boolean isProductionMode = false;
+
+		if (profilesActive != null && profilesActive.equals("production")) {
+		  isProductionMode = true;
+		}
+				
+		if ( isProductionMode ) {
+		  MysqlUtil.setDBInfo("127.0.0.1", "sbsstLocal", "sbs123414", "jspCommunity");
+		}
+		else {
+		  MysqlUtil.setDBInfo("127.0.0.1", "sbsst", "sbs123414", "jspCommunity");			
+		}
 
 		String controllerTypeName = requestUriBits[2];
 		String controllerName = requestUriBits[3];
